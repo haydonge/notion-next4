d637ae4a36370155bdd5deaa1c33be1278fb6bf8
haydonge <93337428+haydonge@users.noreply.github.com>
2023-07-19 10:23:39 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,24 +1,24 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
 
-  SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2023-04-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到海外代孕库', url: 'https://ivf.geoivf.com' },
+    { title: '访问文档中心获取更多帮助', url: 'https://docs.geoivf.com' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
+  HERO_TITLE_1: '分享代孕',
+  HERO_TITLE_2: '与海外生殖',
+  HERO_TITLE_3: 'geoivf.com',
   HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HERO_TITLE_5: '自卵捐卵,单身夫妻代孕',
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: '使用教程', url: '/tag/实用教程' },
+  HERO_CATEGORY_1: { title: '医学名词', url: '/tag/医学名词' },
+  HERO_CATEGORY_2: { title: '代孕法律', url: '/tag/代孕' },
+  HERO_CATEGORY_3: { title: '工具文件', url: '/tag/工具' },
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   INFOCARD_GREETINGS: [
